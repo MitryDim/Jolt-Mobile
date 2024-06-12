098c778558523df15b23a716df9e0bdb058b1cc4
MitryDim <dimcorb@gmail.com>
2024-06-12 12:09:22 +0200

    chore: Mettre à jour les dépendances react-native-svg et expo-dev-client

--- src/containers/ChoiceAddressScreen.js
+++ src/containers/ChoiceAddressScreen.js
@@ -1,8 +1,28 @@
-import { View, Text, StyleSheet } from "react-native";
-import React,{useRef,useState,useEffect} from "react";
-import MapView, { AnimatedRegion, Animated, Circle,Marker, MapMarker,MarkerAnimated } from "react-native-maps";
-import * as Location from 'expo-location';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Animated,
+  Dimensions,
+  Platform,
+} from "react-native";
+import React, {
+  useRef,
+  useState,
+  useEffect,
+  useLayoutEffect,
+  useCallback,
+} from "react";
+import MapView, {
+  AnimatedRegion,
+  Circle,
+  Marker,
+  MapMarker,
+  MarkerAnimated,
+} from "react-native-maps";
+import * as Location from "expo-location";
 import Arrow from "../components/Arrow";
+import { AnimatedMapView } from "react-native-maps/lib/MapView";
 import {
   SafeAreaView,
   useSafeAreaInsets,
@@ -12,29 +32,99 @@ const COLOR = {
   paperBlue200: { color: "#AFCCF9" },
 };
 const ChoiceAddressScreen = () => {
-
-
   const mapRef = useRef(null);
+  const heading = new Animated.Value(0);
+  const coordinates = new AnimatedRegion({
+    latitude: 0,
+    longitude: 0,
+  });
 
-  const [cameraHeading, setCameraHeading] = React.useState(0);
-  function updateCameraHeading() {
-    const map = mapRef.current;
-    map.getCamera().then((info) => {
-      setCameraHeading(info.heading);
-    });
+  const { width, height } = Dimensions.get("window");
+
+  const insets = useSafeAreaInsets();
+
+  const MERCATOR_OFFSET = Math.pow(2, 28);
+  const MERCATOR_RADIUS = MERCATOR_OFFSET / Math.PI;
+
+  function mercatorLatitudeToY(latitude) {
+    return Math.round(
+      MERCATOR_OFFSET -
+        (MERCATOR_RADIUS *
+          Math.log(
+            (1 + Math.sin(latitude * (Math.PI / 180))) /
+              (1 - Math.sin(latitude * (Math.PI / 180)))
+          )) /
+          2
+    );
   }
-   const insets = useSafeAreaInsets();
-    const [location, setLocation] = useState({
-      accuracy: 0,
-      altitude: 0,
-      altitudeAccuracy: 0,
-      heading: 0,
-      latitude: 0,
-      longitude: 0,
-      speed: 0,
-    });
-console.log(location)
-  useEffect(() => {
+
+  function mercatorLongitudeToX(longitude) {
+    return Math.round(
+      MERCATOR_OFFSET + (MERCATOR_RADIUS * longitude * Math.PI) / 180
+    );
+  }
+
+  function mercatorXToLongitude(x) {
+    return (((x - MERCATOR_OFFSET) / MERCATOR_RADIUS) * 180) / Math.PI;
+  }
+
+  function mercatorYToLatitude(y) {
+    return (
+      ((Math.PI / 2 -
+        2 * Math.atan(Math.exp((y - MERCATOR_OFFSET) / MERCATOR_RADIUS))) *
+        180) /
+      Math.PI
+    );
+  }
+
+  function mercatorAdjustLatitudeByOffsetAndZoom(latitude, offset, zoom) {
+    return mercatorYToLatitude(
+      mercatorLatitudeToY(latitude) + (offset << (21 - zoom))
+    );
+  }
+
+  function mercatorAdjustLongitudeByOffsetAndZoom(longitude, offset, zoom) {
+    return mercatorXToLongitude(
+      mercatorLongitudeToX(longitude) + (offset << (21 - zoom))
+    );
+  }
+
+  function mercatorDegreeDeltas(latitude, longitude, width, height, zoom) {
+    if (!zoom) {
+      zoom = 20;
+    }
+
+    const deltaX = width / 2;
+    const deltaY = height / 4;
+
+    const northLatitude = mercatorAdjustLatitudeByOffsetAndZoom(
+      latitude,
+      deltaY * -1,
+      zoom
+    );
+    const westLongitude = mercatorAdjustLongitudeByOffsetAndZoom(
+      longitude,
+      deltaX * -1,
+      zoom
+    );
+    const southLatitude = mercatorAdjustLatitudeByOffsetAndZoom(
+      latitude,
+      deltaY,
+      zoom
+    );
+    const eastLongitude = mercatorAdjustLongitudeByOffsetAndZoom(
+      longitude,
+      deltaY,
+      zoom
+    );
+
+    const latitudeDelta = Math.abs(northLatitude - southLatitude);
+    const longitudeDelta = Math.abs(eastLongitude - westLongitude);
+
+    return { latitudeDelta, longitudeDelta };
+  }
+
+  useLayoutEffect(() => {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== "granted") {
@@ -45,67 +135,126 @@ console.log(location)
       Location.watchPositionAsync(
         {
           accuracy: Location.Accuracy.High,
-          timeInterval: 1000,
-          distanceInterval: 1,
+          timeInterval: 200,
+          distanceInterval: 0.1,
         },
         (newLocation) => {
           let { coords } = newLocation;
-          console.log("newLocation", coords);
-          setLocation(coords);
+
+          const map = mapRef.current;
+          coordinates
+            .timing({
+              latitude: coords.latitude,
+              longitude: coords.longitude,
+            })
+            .start();
+
+          updateCamera(map, coords, heading);
         }
       );
     })();
   }, []);
 
+  const SCREEN_HEIGHT_RATIO = height / 1920; // 1920 est un exemple de hauteur d'écran de référence
+  const BASE_OFFSET = -0.005 * SCREEN_HEIGHT_RATIO; // Ajuster 0.002 si nécessaire
+
+  const getOffset = (zoom, heading) => {
+    const offset = BASE_OFFSET / Math.pow(2, zoom ); // Ajustement basé sur le zoom
+    const radHeading = heading * (Math.PI / 180); // Convertir le heading en radians
+
+    // Calculer le décalage basé sur le heading
+    const offsetLatitude = offset * Math.cos(radHeading);
+    const offsetLongitude = offset * Math.sin(radHeading);
+
+    // Inverser le décalage pour le garder en bas de l'écran
+    return {
+      offsetLatitude: -offsetLatitude,
+      offsetLongitude: -offsetLongitude,
+    };
+  };
+
+  const updateCamera = useCallback((map, coordinates, heading) => {
+    const { latitudeDelta, longitudeDelta } = mercatorDegreeDeltas(
+      coordinates.latitude,
+      coordinates.longitude,
+      width,
+      height,
+      Platform.OS === "ios" ? 20 : 19
+    );
+    const { offsetLatitude, offsetLongitude } = getOffset(
+      Platform.OS === "ios" ? 3 : 2,
+      coordinates.heading
+    );
+    console.log("Platform.OS", Platform.OS);
+    map.animateCamera(
+      {
+        center: {
+          latitude: coordinates.latitude + offsetLatitude,
+          longitude: coordinates.longitude + offsetLongitude,
+        },
+        heading: coordinates.heading,
+        pitch: Platform.OS === "ios" ? 60 : 75,
+        altitude: Platform.OS === "ios" ? 90 : 70,
+        zoom: Platform.OS === "ios" ? 0 : 19,
+        latitudeDelta: latitudeDelta,
+        longitudeDelta: longitudeDelta,
+      },
+      { duration: 500 }
+    );
+
+    setTimeout(async () => {
+      const cam = await map.getCamera();
+      Animated.timing(heading, {
+        toValue: coordinates.heading - cam?.center ? cam?.center?.heading : 0,
+        duration: 400,
+        useNativeDriver: true,
+      }).start();
+    }, 2000);
+  }, []);
+
   return (
     <View style={{ flex: 1, marginBottom: 60, paddingBottom: insets.bottom }}>
       <View style={{ borderRadius: 30, backgroundColor: "white" }}></View>
-      <MapView
+      <AnimatedMapView
         style={styles.map}
         ref={mapRef}
         showsUserLocation={false}
-        followsUserLocation={true}
+        followsUserLocation={false}
         zoomEnabled={true}
         zoomControlEnabled={true}
-        zoomTapEnabled={true}
+        zoomTapEnabled={false}
         pitchEnabled={true}
-        onTouchEnd={() => {
-          updateCameraHeading();
-        }}
-        onTouchCancel={() => {
-          updateCameraHeading();
-        }}
-        onTouchStart={() => {
-          updateCameraHeading();
-        }}
-        onTouchMove={() => {
-          updateCameraHeading();
-        }}
+        showsBuildings={true}
       >
         <MarkerAnimated
-          coordinate={{
-            latitude: location.latitude,
-            longitude: location.longitude,
-          }}
-          anchor={{ x: 0.5, y: 0.5 }}
+          coordinate={coordinates}
+          flat={false}
+          anchor={{ x: 0.5, y: 0.2 }}
         >
-          <View
+          <Animated.View
             style={{
-              transform: [{ rotate: `${location.heading - cameraHeading}deg` }],
+              transform: [
+                {
+                  rotate: heading.interpolate({
+                    inputRange: [0, 360],
+                    outputRange: ["0deg", "360deg"],
+                  }),
+                },
+              ],
             }}
           >
             <Arrow />
-          </View>
+          </Animated.View>
         </MarkerAnimated>
-      </MapView>
+      </AnimatedMapView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   map: {
-    width:"100%",
-    height:'100%',
+    width: "100%",
+    height: "100%",
   },
 });
 
